387f5f3e550e48b4f97addbc273801ff05618805
Joe Ma <joema.dev@gmail.com>
2026-05-27 16:16:46 -0700

    Commit articles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'mrjoema';
+export const SITE_TITLE = "Joe Ma's Tech Blog";
 export const SITE_DESCRIPTION = 'Articles, showcases, and experiments by Hok Yin Ma.';
